5a696e06d33c79f5342cc4044d176098786efe6c
Trey Mitra <shobhan.mitra@gmail.com>
2012-03-10 14:48:54 -0800

    new Rails app generated by Rails Apps Composer gem

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rails3DeviseRspecCucumber::Application.config.secret_token = '6f43eb85f69fd02b5676b8fa54d60f0c4753fe8fec241691fc4b84b9c3cd720212173caef90a354f103e8540b8b2cbb77933f6d746b72338d45821702ed935cd'
